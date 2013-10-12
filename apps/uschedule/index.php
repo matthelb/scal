ea04762ec94fda3645b24e579cc9869e6bbd2753
Matt Burke <matt_burke@live.com>
2013-10-11 23:24:53 -0700

    Create uschedule alias and app. Add .gitignore file

---
+++ apps/uschedule/index.php
@@ -0,0 +1,8 @@
+<?php
+$ch = curl_init("http://web-app.usc.edu/ws/soc/api/classes/phys/20133");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
+curl_close($ch);
+var_dump($result);
+var_dump(json_decode($result, true));
+?>
