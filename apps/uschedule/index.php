45d4fb85cce998a2d2b8d17a042b602ab4cab1c5
ananthmo <ananthmo@usc.edu>
2013-10-12 21:39:28 -0700

    Added CSS to index.php, added logo & background.

--- apps/uschedule/index.php
+++ apps/uschedule/index.php
@@ -1,27 +1,67 @@
-<?php
-$curl = curl_init();
-	curl_setopt($curl,CURLOPT_URL,'http://web-app.usc.edu/ws/soc/api/depts/20133');
-	curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
-	$result = curl_exec($curl);
-	curl_close($curl);
-	$classes = json_decode($result,true);
-	var_dump($classes);
-	for($i=0;$i<sizeof($classes['department']);$i++){
-		echo $classes['department'][$i]['name'];
-		echo "<br />";
-	}*/
-	$curl2 = curl_init();
-	curl_setopt($curl2, CURLOPT_URL, 'http://web-app.usc.edu/ws/soc/api/classes/csci/20133');
-	curl_setopt($curl2,CURLOPT_RETURNTRANSFER,true);
-	$result2 = curl_exec($curl2);
-	curl_close($curl2);
-	$classes2 = json_decode($result2, true);
-	var_dump($classes2['OfferedCourses']['course']);
-	for($i=0;$i<sizeof($classes2['OfferedCourses']['course']);$i++){
-		for($j=0;$j<sizeof($classes2['OfferedCourses']['course'][$i]['CourseData']);$j++){
-		echo $classes2['OfferedCourses']['course'][$i]['CourseData']['title'];
-		//echo $classes2['OfferedCourses']['course'][$i]['CourseData']['SectionData'][$j];
-		echo "<br />";
+<?php require_once("db_populate.php"); ?>
+<html lang="en">
+<head>
+	<title>SCal to Google</title>
+	<style type="text/css">
+		body {
+			background-color: #FFFF66;
+			background-image:url('stripes.jpg'); }
+		</style>
+	</head>
+	<div id="logo"> <img src="logo.gif"/> </div>
+	<style type = "text/css">
+	#logo{
+		text-align: center;
+		}
+	</style>
+	<div id="main_section">
+	<body>
+		<form action ="index.php">
+		<style type = "text/css">
+			select{
+				width:500px;
+				text-align:center;
+			}
+		</style>
+			<select id="department">
+				<option value="">Department</option>
+				<?php populate_dpts(); ?>
+			</select><br />
+			<?php?>
+			<select id="course">
+				<option value="">Course</option>
+				<?php populate_courses(); ?>
+			</select><br />
+			<select id="section">
+				<option value ="">Section</option>
+				<?php populate_sections(); ?>
+			</select><br />
+			<input type="submit" name="go" value="submit">
+		</form>
+		<script src="jquery.js" type="text/javascript" charset="utf-8"></script>
+		<script src="jquery.chained.mini.js" type="text/javascript" charset="utf-8"></script>
+		<script type="text/javascript" charset="utf-8">
+			$("#course").chained("#department");
+			$("#section").chained("#course");
+		</script>
+		
+	</body>
+	</div>
+	<style type="text/css">
+	#main_section{
+		text-align:center;
 	}
-}
-?>
+	</style>
+	<div id="socialMedia"> 
+		<div id="Facebook"> 
+		<a href="http://www.facebook.com">
+		<img src="fnl_facebook_logo.png" height="45" width = "90"> 
+		</a>
+		</div>
+		</div>
+	<style type="text/css">
+	#socialMedia{
+		text-align:bottom;
+	}
+	</style>
+	</html>
