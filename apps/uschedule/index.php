272d4a140930a94783354f05ed6f63a2e163f710
ananthmo <ananthmo@usc.edu>
2013-10-13 16:59:44 -0700

    ...

--- apps/uschedule/index.php
+++ apps/uschedule/index.php
@@ -4,6 +4,9 @@
 <head>
 	<meta charset="utf-8">
 	<title>SCal to Google</title>
+	<link rel="icon" 
+      type="icon" 
+      href="/img/favicon.ico"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<meta name="author" content="">
